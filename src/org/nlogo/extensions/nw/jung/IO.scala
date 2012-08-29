18e4245384062ac7b2b4cff60435a4b3d55e123d
Nicolas Payette <nicolaspayette@gmail.com>
2012-08-29 16:31:07 -0500

    add error msg for save/load in applet

--- src/org/nlogo/extensions/nw/jung/IO.scala
+++ src/org/nlogo/extensions/nw/jung/IO.scala
@@ -5,22 +5,27 @@ import org.nlogo.agent.Link
 import org.nlogo.agent.Turtle
 import edu.uci.ics.jung
 import java.util.Random
+import org.nlogo.workspace.AbstractWorkspace
+import org.nlogo.api.ExtensionException
 
 object Matrix {
 
   def save(graph: jung.graph.Graph[Turtle, Link], filename: String) {
+    if (org.nlogo.workspace.AbstractWorkspace.isApplet)
+      throw new ExtensionException("Cannot save matrix file when in applet mode.")
     new jung.io.MatrixFile[Turtle, Link](
-      null, // TODO: provide weight key (null means 1) 
+      null, // TODO: provide weight key (null means 1) (issue #19) 
       null, null, null // no factories needed for save
       ).save(graph, filename)
   }
 
-  def load(filename: String, turtleBreed: AgentSet, linkBreed: AgentSet, rng: Random) =
-    DummyGraph.importToNetLogo(
-      new jung.io.MatrixFile(
-        null, // TODO: provide weight key (null means 1) 
-        DummyGraph.factoryFor(linkBreed), DummyGraph.vertexFactory, DummyGraph.edgeFactory)
-        .load(filename),
-      turtleBreed, linkBreed, rng)
-
+  def load(filename: String, turtleBreed: AgentSet, linkBreed: AgentSet, rng: Random) = {
+    if (org.nlogo.workspace.AbstractWorkspace.isApplet)
+      throw new ExtensionException("Cannot load matrix file when in applet mode.")
+    val matrixFile = new jung.io.MatrixFile(
+      null, // TODO: provide weight key (null means 1) (issue #19) 
+      DummyGraph.factoryFor(linkBreed), DummyGraph.vertexFactory, DummyGraph.edgeFactory)
+    val graph = matrixFile.load(filename)
+    DummyGraph.importToNetLogo(graph, turtleBreed, linkBreed, rng)
+  }
 }
