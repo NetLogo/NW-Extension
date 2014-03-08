ab291ed8c1402f25733ac39f2e235f080d994d09
Bryan Head <bryan.head@gmail.com>
2014-03-08 01:03:15 -0600

    Make cache selection a little smarter

--- src/main/org/nlogo/extensions/nw/algorithms/Graph.scala
+++ src/main/org/nlogo/extensions/nw/algorithms/Graph.scala
@@ -20,21 +20,41 @@ trait Graph {
   private def getSingleDestTraversal(dest: Turtle): Iterator[Turtle] = {
     singleDestTraversals.getOrElseUpdate(dest, cachingBFS(dest, true, getSuccessor))
   }
+
+  private var lastSource: Option[Turtle] = None
+  private var lastDest: Option[Turtle] = None
+
+  /**
+   * Attempts to expand the cache with the least duplicated amount of work possible. It tries to detect users doing
+   * single-source and single-destination. Failing that, it expands the caching BFS that is the furthest along, since
+   * that one is guaranteed to finish the quickest.
+   * @param source
+   * @param dest
+   */
   private def expandBestTraversal(source: Turtle, dest: Turtle) {
     val sourceTraversal = getSingleSourceTraversal(source)
     val destTraversal = getSingleDestTraversal(dest)
     // If one doesn't have a next, the nodes are disconnected
     if (sourceTraversal.hasNext && destTraversal.hasNext) {
-      val sourcePosition = sourceTraversal.next()
-      val destPosition = destTraversal.next()
-      if (sourcePosition != dest && destPosition != source) {
-        if (distances((source, sourcePosition)) > distances((destPosition, dest))) {
-          sourceTraversal find { _ == dest }
-        } else {
-          destTraversal find { _ == source }
+      if (lastSource.exists(_ == source)) {
+        sourceTraversal find { _ == dest }
+      } else if (lastDest.exists(_ == dest)) {
+        destTraversal find { _ == source }
+      } else {
+        val sourcePosition = sourceTraversal.next()
+        val destPosition = destTraversal.next()
+        if (sourcePosition != dest && destPosition != source) {
+          if (distances((source, sourcePosition)) >= distances((destPosition, dest))) {
+            sourceTraversal find { _ == dest }
+          }
+          if (distances((source, sourcePosition)) <= distances((destPosition, dest))) {
+            destTraversal find { _ == source }
+          }
         }
       }
     }
+    lastSource = Some(source)
+    lastDest = Some(dest)
   }
 
   private def getPredecessor(dest: Turtle, source: Turtle): ArrayBuffer[Turtle] = {
@@ -58,10 +78,13 @@ trait Graph {
     }
   }
 
+  private def cachedPath(source: Turtle, dest: Turtle): Option[List[Turtle]] =
+    cachedPath(getSuccessor, source, dest) orElse cachedPath(getPredecessor, dest, source).map(_.reverse)
+
   def path(source: Turtle, dest: Turtle): Option[Iterable[Turtle]] = {
-    cachedPath(getSuccessor, source, dest) orElse cachedPath(getPredecessor, dest, source).map(_.reverse) orElse {
+    cachedPath(source, dest) orElse {
       expandBestTraversal(source, dest)
-      cachedPath(getSuccessor, source, dest)
+      cachedPath(source, dest)
     }
   }
 
@@ -84,6 +107,9 @@ trait Graph {
     Turtle) => ArrayBuffer[Turtle]): Iterator[Turtle] = {
     val dists = mutable.Map[(Turtle,Turtle), Int]()
     dists((start, start)) = 0
+
+    // note that I can't use the global distances cache to detect visited nodes since
+    // the same slot can be filled by either a BFS or reverse BFS.
     distances((start, start)) = 0
     Iterator.iterate(List(start))((last) => {
       var layer: List[Turtle] = List()
