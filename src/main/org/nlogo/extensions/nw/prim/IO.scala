c786a9582fb77cd451671ed6484e897de3848a55
Bryan Head <bryan.head@gmail.com>
2015-01-19 09:57:47 -0600

    Add breed specification to load

--- src/main/org/nlogo/extensions/nw/prim/IO.scala
+++ src/main/org/nlogo/extensions/nw/prim/IO.scala
@@ -15,7 +15,7 @@ import org.nlogo.extensions.nw.gephi.GephiUtils.withNWLoaderContext
 import scala.collection.JavaConverters._
 
 class Load extends TurtleCreatingCommand {
-  override def getSyntax = commandSyntax(Array(StringType, CommandBlockType | OptionalType))
+  override def getSyntax = commandSyntax(Array(StringType, TurtlesetType, LinksetType, CommandBlockType | OptionalType))
 
   def createTurtles(args: Array[api.Argument], context: Context): TraversableOnce[Turtle] = withNWLoaderContext {
     val importer = Lookup.getDefault.lookup(classOf[ImportController])
@@ -23,18 +23,20 @@ class Load extends TurtleCreatingCommand {
     val world = ws.world
     val fm = ws.fileManager
     val file = new File(fm.attachPrefix(args(0).getString))
+    val turtleBreed = args(1).getAgentSet.requireTurtleBreed
+    val linkBreed = args(2).getAgentSet.requireLinkBreed
     val container = importer.importFile(file)
     val nodes: Iterable[NodeDraftGetter] = container.getUnloader.getNodes.asScala
     val edges: Iterable[EdgeDraftGetter] = container.getUnloader.getEdges.asScala
 
     val nodeToTurtle: Map[NodeDraftGetter, Turtle] = nodes zip nodes.map {
-      node => createTurtle(world.turtles, ws.mainRNG): Turtle
+      node => createTurtle(turtleBreed, ws.mainRNG): Turtle
     } toMap
 
     val edgesToLinks: Map[EdgeDraftGetter, Link] = edges zip edges.map { edge =>
       val source = nodeToTurtle(edge.getSource)
       val target = nodeToTurtle(edge.getTarget)
-      world.linkManager.createLink(source, target, world.links)
+      world.linkManager.createLink(source, target, linkBreed)
     } toMap
 
     nodeToTurtle.values
