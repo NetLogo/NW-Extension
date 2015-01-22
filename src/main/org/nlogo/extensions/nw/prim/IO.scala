1010187bdafa521cf55c8428c2fd6198b9823ead
Bryan Head <bryan.head@gmail.com>
2015-01-21 21:11:32 -0600

    Add support for labels and colors in loading

--- src/main/org/nlogo/extensions/nw/prim/IO.scala
+++ src/main/org/nlogo/extensions/nw/prim/IO.scala
@@ -1,20 +1,24 @@
 package org.nlogo.extensions.nw.prim
 
+import java.awt.Color
 import java.io.File
 
 import org.gephi.io.importer.api.{EdgeDraftGetter, ImportController, NodeDraftGetter}
 import org.nlogo.agent.{Link, Turtle}
 import org.nlogo.api
-import org.nlogo.api.Context
 import org.nlogo.api.Syntax._
+import org.nlogo.api.{Context, LogoList, AgentVariableNumbers}
+import AgentVariableNumbers._
 import org.nlogo.extensions.nw.NetworkExtensionUtil.{TurtleCreatingCommand, _}
+import org.nlogo.extensions.nw.gephi.GephiUtils.withNWLoaderContext
 import org.nlogo.nvm.ExtensionContext
 import org.openide.util.Lookup
-import org.nlogo.extensions.nw.gephi.GephiUtils.withNWLoaderContext
 
 import scala.collection.JavaConverters._
 
 class Load extends TurtleCreatingCommand {
+  private type JDouble = java.lang.Double
+
   override def getSyntax = commandSyntax(Array(StringType, TurtlesetType, LinksetType, CommandBlockType | OptionalType))
 
   def createTurtles(args: Array[api.Argument], context: Context): TraversableOnce[Turtle] = withNWLoaderContext {
@@ -25,12 +29,20 @@ class Load extends TurtleCreatingCommand {
     val file = new File(fm.attachPrefix(args(0).getString))
     val turtleBreed = args(1).getAgentSet.requireTurtleBreed
     val linkBreed = args(2).getAgentSet.requireLinkBreed
-    val container = importer.importFile(file)
-    val nodes: Iterable[NodeDraftGetter] = container.getUnloader.getNodes.asScala
-    val edges: Iterable[EdgeDraftGetter] = container.getUnloader.getEdges.asScala
+    val unloader = importer.importFile(file).getUnloader
+    val nodes: Iterable[NodeDraftGetter] = unloader.getNodes.asScala
+    val edges: Iterable[EdgeDraftGetter] = unloader.getEdges.asScala
 
     val nodeToTurtle: Map[NodeDraftGetter, Turtle] = nodes zip nodes.map {
-      node => createTurtle(turtleBreed, ws.mainRNG): Turtle
+      node => {
+        val turtle = createTurtle(turtleBreed, ws.mainRNG)
+        Option(node.getLabel)      foreach (l => turtle.setTurtleVariable(VAR_LABEL, l))
+        Option(node.getLabelColor) foreach (c => turtle.setTurtleVariable(VAR_LABELCOLOR, convertColor(c)))
+        Option(node.getColor)      foreach (c => turtle.setTurtleVariable(VAR_COLOR, convertColor(c)))
+        // Note that node's have a getSize. This does not correspond to the `size` attribute in files so should not be
+        // used. BCH 1/21/2015
+        turtle
+      }
     } toMap
 
     val edgesToLinks: Map[EdgeDraftGetter, Link] = edges zip edges.map { edge =>
@@ -40,6 +52,14 @@ class Load extends TurtleCreatingCommand {
     } toMap
 
     nodeToTurtle.values
+  }
+
+  private def convertColor(c: Color): LogoList = {
+    val l = LogoList(c.getRed.toDouble: JDouble,
+                     c.getGreen.toDouble: JDouble,
+                     c.getBlue.toDouble: JDouble)
+    if (c.getAlpha != 255) l.lput(c.getAlpha.toDouble: JDouble)
+    l
   }
 }
 
