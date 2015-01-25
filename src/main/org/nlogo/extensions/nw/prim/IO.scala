6b25a4c64ae2def1f7cdee61763f0fc54c447764
Bryan Head <bryan.head@gmail.com>
2015-01-25 11:56:24 -0600

    Cleanup

--- src/main/org/nlogo/extensions/nw/prim/IO.scala
+++ src/main/org/nlogo/extensions/nw/prim/IO.scala
@@ -7,36 +7,33 @@ import org.gephi.data.attributes.`type`.DynamicType
 import org.gephi.data.attributes.api.AttributeRow
 import org.gephi.io.importer.api.EdgeDraft.EdgeType
 import org.gephi.io.importer.api.{EdgeDraftGetter, ImportController, NodeDraftGetter}
-import org.nlogo.agent.{Link, Turtle, AgentSet}
+import org.nlogo.agent.{Agent, Turtle, AgentSet, World}
 import org.nlogo.api
-import org.nlogo.api.AgentVariableNumbers._
 import org.nlogo.api.{ExtensionException, LogoList}
 import org.nlogo.api.Syntax._
 import org.nlogo.extensions.nw.NetworkExtensionUtil._
+import org.nlogo.extensions.nw.gephi.GephiUtils
 import org.nlogo.nvm.ExtensionContext
 import org.openide.util.Lookup
 
 import scala.collection.JavaConverters._
-import scala.collection.mutable.ArrayBuffer
 
 class Load extends TurtleAskingCommand {
   private type JDouble = java.lang.Double
 
   override def getSyntax = commandSyntax(Array(StringType, TurtlesetType, LinksetType, CommandBlockType | OptionalType))
 
-  override def perform(args: Array[api.Argument], context: api.Context) = {
+  override def perform(args: Array[api.Argument], context: api.Context) = GephiUtils.withNWLoaderContext {
     val importer = Lookup.getDefault.lookup(classOf[ImportController])
     val ws = context.asInstanceOf[ExtensionContext].workspace
     val world = ws.world
     val turtleBreeds = world.program.breeds.asScala.toMap
     val linkBreeds = world.program.linkBreeds.asScala.toMap
-    val turtlesOwn = world.program.turtlesOwn
-    val linksOwn = world.program.linksOwn
     val fm = ws.fileManager
-    val file = new File(fm.attachPrefix(args(0).getString))
     val turtleBreed = args(1).getAgentSet.requireTurtleBreed
     val linkBreed = args(2).getAgentSet.requireLinkBreed
 
+    val file = new File(fm.attachPrefix(args(0).getString))
     val parser = importer.getFileImporter(file)
     val unloader = try using(new FileReader(file))(r => importer.importFile(r, parser).getUnloader)
                    catch { case e: IOException => throw new ExtensionException(e) }
@@ -45,7 +42,7 @@ class Load extends TurtleAskingCommand {
 
     val nodeToTurtle: Map[NodeDraftGetter, Turtle] = nodes zip nodes.map {
       node => {
-        val attrs = getAttributes(node.getAttributeRow()) ++
+        val attrs = getAttributes(node.getAttributeRow) ++
                     pair("LABEL", node.getLabel) ++
                     pair("LABEL-COLOR", node.getLabelColor) ++
                     pair("COLOR", node.getColor)
@@ -54,34 +51,23 @@ class Load extends TurtleAskingCommand {
 
         val breed = getBreed(attrs, turtleBreeds).getOrElse(turtleBreed)
         val turtle = createTurtle(breed, ws.mainRNG)
-        (attrs - "BREED") foreach { case (k: String, v: AnyRef) =>
-          val i = world.indexOfVariable(turtle, k)
-          if (i != -1) turtle.setVariable(i, v)
-        }
+        (attrs - "BREED") foreach (setAttribute(world, turtle) _).tupled
         turtle
       }
     } toMap
 
-    val badEdges: ArrayBuffer[EdgeDraftGetter] = ArrayBuffer()
-    val edgesToLinks: Map[EdgeDraftGetter, Seq[Link]] = edges zip edges.map { edge =>
+    val badEdges = edges.map { edge =>
       val source = nodeToTurtle(edge.getSource)
       val target = nodeToTurtle(edge.getTarget)
       // There are three gephi edge types: directed, undirected, and mutual. Mutual is pretty much just indicating that
       // and edge goes both ways/there are two edges in either direction, so we treat it as either. BCH 1/22/2015
-      val gephiDirected = edge.getType == EdgeType.DIRECTED
-      val gephiUndirected = edge.getType == EdgeType.UNDIRECTED
-      val attrs = getAttributes(edge.getAttributeRow()) ++
+      val attrs = getAttributes(edge.getAttributeRow) ++
                   pair("LABEL", edge.getLabel) ++
                   pair("LABEL-COLOR", edge.getLabelColor) ++
                   pair("COLOR", edge.getColor) ++
-                  pair("WEIGHT", edge.getWeight: java.lang.Double)
+                  pair("WEIGHT", edge.getWeight.toDouble: java.lang.Double)
 
       val breed = getBreed(attrs, linkBreeds).getOrElse(linkBreed)
-      if (breed.isDirected == breed.isUndirected) {
-        breed.setDirected(gephiDirected)
-      } else if ((breed.isDirected && gephiUndirected) || (breed.isUndirected && gephiDirected)) {
-        badEdges.append(edge)
-      }
 
       val links = List(world.linkManager.createLink(source, target, breed)) ++ {
         if (breed.isDirected && edge.getType == EdgeType.MUTUAL)
@@ -89,14 +75,20 @@ class Load extends TurtleAskingCommand {
         else
           None
       }
-      links foreach { l =>
-        (attrs - "BREED") foreach { case (k: String, v: AnyRef) =>
-          val i = world.indexOfVariable(l, k)
-          if (i != -1) l.setVariable(i, v)
-        }
+      links foreach { l => (attrs - "BREED") foreach (setAttribute(world, l) _).tupled }
+
+      val gephiDirected = edge.getType == EdgeType.DIRECTED
+      val gephiUndirected = edge.getType == EdgeType.UNDIRECTED
+      if (breed.isDirected == breed.isUndirected) {
+        // This happens when the directedness of the default breed hasn't been set yet
+        breed.setDirected(gephiDirected)
+        None
+      } else if ((breed.isDirected && gephiUndirected) || (breed.isUndirected && gephiDirected)) {
+        Some(edge)
+      } else {
+        None
       }
-      links
-    } toMap
+    }.collect{case Some(e: EdgeDraftGetter) => e}
 
     askTurtles(nodeToTurtle.values, context)
 
@@ -139,6 +131,11 @@ class Load extends TurtleAskingCommand {
     case d: DynamicType[_] => LogoList.fromIterator(d.getValues.asScala.map(x => convertAttribute(x)).iterator)
     case a: Array[_] => LogoList.fromIterator(a.map(x => convertAttribute(x)).iterator)
     case x => x.toString
+  }
+
+  private def setAttribute(world: World, agent: Agent)(name: String, value: AnyRef): Unit = {
+    val i = world.indexOfVariable(agent, name)
+    if (i != -1) agent.setVariable(i, value)
   }
 }
 
