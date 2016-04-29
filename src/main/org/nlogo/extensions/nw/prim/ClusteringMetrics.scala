d1bb157172f05b524eba79ed5a367bbf3b709944
Robert Grider <robert.grider@northwestern.edu>
2016-04-29 06:58:35 -0500

    ClusteringMetrics prims use core.Syntax

--- src/main/org/nlogo/extensions/nw/prim/ClusteringMetrics.scala
+++ src/main/org/nlogo/extensions/nw/prim/ClusteringMetrics.scala
@@ -1,11 +1,12 @@
 package org.nlogo.extensions.nw.prim
 
-import org.nlogo.api, api.Syntax._
+import org.nlogo.api
+import org.nlogo.core.Syntax._
 import org.nlogo.agent
 import org.nlogo.extensions.nw.{GraphContext, GraphContextProvider}
 
 class ClusteringCoefficient(gcp: GraphContextProvider) extends api.Reporter {
-  override def getSyntax = reporterSyntax(NumberType, "-T--")
+  override def getSyntax = reporterSyntax(ret = NumberType, agentClassString = "-T--")
   override def report(args: Array[api.Argument], context: api.Context) = {
     val graph = gcp.getGraphContext(context.getAgent.world)
     graph.clusteringCoefficient(context.getAgent.asInstanceOf[agent.Turtle]): java.lang.Double
