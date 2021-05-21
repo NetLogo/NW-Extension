0cb2b5f82551e2e1eca99fabb3706bef03c65de7
Aaron Andre Brandes <aaron.brandes@northwestern.edu>
2021-05-21 13:59:40 -0400

    for Java 9 - remove uses of Applet

--- src/main/org/nlogo/extensions/nw/jung/io/Matrix.scala
+++ src/main/org/nlogo/extensions/nw/jung/io/Matrix.scala
@@ -20,8 +20,6 @@ import edu.uci.ics.jung.algorithms.matrix.GraphMatrixOperations
 object Matrix {
 
   def save(graph: jung.graph.Graph[Turtle, Link], filename: String) {
-    if (org.nlogo.workspace.AbstractWorkspace.isApplet)
-      throw new ExtensionException("Cannot save matrix file when in applet mode.")
     /* This is almost a line for line copy of jung.io.MatrixFile.save, the major
      * difference being that it explicitly uses the US locale to make sure entries
      * use the dot decimal separator (see issue #69) */
@@ -43,10 +41,8 @@ object Matrix {
   }
 
   def load(filename: String, turtleBreed: AgentSet, linkBreed: AgentSet, world: World, rng: MersenneTwisterFast) = {
-    if (org.nlogo.workspace.AbstractWorkspace.isApplet)
-      throw new ExtensionException("Cannot load matrix file when in applet mode.")
     val matrixFile = new jung.io.MatrixFile(
-      null, // TODO: provide weight key (null means 1) (issue #19) 
+      null, // TODO: provide weight key (null means 1) (issue #19)
       factoryFor(linkBreed), DummyGraph.vertexFactory, DummyGraph.edgeFactory)
     val graph = try {
       matrixFile.load(filename)
